bda847786657f41d650329250c0eaa520afd53e3
matheus-codemize <matheus.codemize@gmail.com>
2020-09-17 13:17:50 -0300

    [add] configuração de variável de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://seu-credito-homolog-a74mkfx2aa-uc.a.run.app',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
