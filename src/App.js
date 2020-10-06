f3019132eae4ba8155da63b696909c5aba7ae8cc
unknown <matheus.codemize@gmail.com>
2020-10-06 12:48:17 -0300

    [fix] nova versão da tela Home (cards)

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ import RegisterClient from './pages/RegisterClient';
 // pages others versions
 import Home1 from './pages/Home1';
 import Home2 from './pages/Home2';
+import Home3 from './pages/Home3';
 
 // resources
 import { routesAgent, routesClient } from './resources/data/sidebar/routes';
@@ -40,6 +41,7 @@ const pages = {
   Proposal,
   Home1,
   Home2,
+  Home3,
 };
 
 function App() {
--- src/resources/data/sidebar/routes.js
+++ src/resources/data/sidebar/routes.js
@@ -1,10 +1,11 @@
 export const routesAgent = [
-  { path: '/home', component: 'Home', name: 'Home', icon: 'fa fa-home' },
+  // { path: '/home', component: 'Home', name: 'Home', icon: 'fa fa-home' },
   { path: '/proposta', component: 'Proposal', name: 'Propostas', icon: 'fab fa-buffer' },
 
   /* others versions */
   { path: '/home1', component: 'Home1', name: 'Home (v1)', icon: 'fa fa-home' },
   { path: '/home2', component: 'Home2', name: 'Home (v2)', icon: 'fa fa-home' },
+  { path: '/home3', component: 'Home3', name: 'Home (v3)', icon: 'fa fa-home' },
   // { path: '/crm', component: 'Crm' },
   // { path: '/simulation', component: 'Simutarion' },
   // { path: '/proposal', component: 'Proposal' }
